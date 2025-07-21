9d068c2153c674897b6108adc0f20bad2a81f3c3
sukesh salivoju <salivojusukesh17@gmail.com>
2025-07-21 14:14:41 -0500

    production ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
     production: true,
-    API_URL: 'https://sukeshsalivoju.vercel.app/api',
-    googleAnalyticsId: 'G-2XR05GXZTD'
+    // API_URL: 'https://sukeshsalivoju.vercel.app/api',
+    googleAnalyticsId: 'G-2XR05GXZTD',
+    emailjs: {
+    serviceID: 'service_sukesh-portfolio', 
+    templateID: 'template_portfolio_cont',   
+    publicKey: 'sRR7MeJZ9fkmHKlaG'      
+  }
 }
+
